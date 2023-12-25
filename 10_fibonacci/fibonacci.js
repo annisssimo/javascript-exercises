934fa679013d8441fd94aa5f93099c8e1c25c18b
annisssimo <anyalar04@gmail.com>
2023-12-25 23:29:17 +0300

    fix: rewrite 10 ex.

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,13 +1,15 @@
-const fibonacci = function(count) { //fibonacci(4) returns the 4th member of the series: 3  (1, 1, 2, 3)
-    if (count < 0) return "OOPS";
-    if (count === 0) return 0;
-    const fibonacciArr = [];
-    fibonacciArr[0] = 1;
-    fibonacciArr[1] = 1;
-    for (let i = 2; i <= count; i++) {
-        fibonacciArr[i] = fibonacciArr[i-1] + fibonacciArr[i-2];
+const fibonacci = function(number) {
+    if (number < 0) return "OOPS";
+    if (number === 0) return 0;
+    if (number === 1 || number === 2) return 1;
+
+    let fibonacciArr = [1, 1];
+
+    for (let i = 2; i < number; i++) {
+        fibonacciArr[i] = fibonacciArr[i - 1] + fibonacciArr[i - 2];
     }
-    return fibonacciArr[count - 1];
+
+    return fibonacciArr[number - 1];
 };
 
 // Do not edit below this line
