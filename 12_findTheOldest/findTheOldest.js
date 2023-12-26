c3846efbcfc39ce025d194a507c7941ae1427eb4
annisssimo <anyalar04@gmail.com>
2023-12-26 13:05:36 +0300

    feat: add 12 ex.

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,19 @@
-const findTheOldest = function() {
+const people = [];
 
-};
+function getAge(person) {
+    const currentDate = new Date();
+  if ('yearOfDeath' in person) {
+    return person.yearOfDeath - person.yearOfBirth;
+  } 
+  else return currentDate.getFullYear() - person.yearOfBirth;
+}
+
+  const findTheOldest = function(people) {
+    people.sort((person1, person2) => getAge(person2) - getAge(person1));
+    return people[0];
+  }
+  
+  console.log(findTheOldest(people));
 
 // Do not edit below this line
 module.exports = findTheOldest;
